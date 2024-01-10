9ce9e51dd1e50218d93fd8f7d3c6f4787299d13d
R. Gomes <rgomes@hotmail.com>
2024-01-10 10:51:10 +0000

    Initial commit

---
+++ src/main/java/com/example/therecipecatalogue/TheRecipeCatalogueApplication.java
@@ -0,0 +1,13 @@
+package com.example.therecipecatalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheRecipeCatalogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheRecipeCatalogueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/therecipecatalogue/TheRecipeCatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.therecipecatalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheRecipeCatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
